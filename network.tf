0505b04cb3df46b706f9742afb997d88ebc3c7a2
shierahall302 <shierahall302@gmail.com>
2024-06-19 10:27:14 -0400

    network

---
+++ network.tf
@@ -0,0 +1,33 @@
+# Configure the Google Cloud Provider
+
+# Define the first VPC
+resource "google_compute_network" "vpc_1" {
+  name                    = "vpc-1"
+  auto_create_subnetworks = false
+  project                 = "certain-gearbox-277406"
+}
+
+# Define the second VPC
+resource "google_compute_network" "vpc_2" {
+  name                    = "vpc-2"
+  auto_create_subnetworks = false
+  project                 = "certain-gearbox-277406"
+}
+
+# Create a subnet for the first VPC
+resource "google_compute_subnetwork" "subnet_1" {
+  name          = "subnet-1"
+  ip_cidr_range = "10.128.0.0/20"
+  region        = "northamerica-northeast1"
+  network       = google_compute_network.vpc_1.id
+  project       = "certain-gearbox-277406"
+}
+
+# Create a subnet for the second VPC
+resource "google_compute_subnetwork" "subnet_2" {
+  name          = "subnet-2"
+  ip_cidr_range = "10.144.0.0/20"
+  region        = "northamerica-northeast1"
+  network       = google_compute_network.vpc_2.id
+  project       = "certain-gearbox-277406"
+}
