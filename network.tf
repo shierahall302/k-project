2d0f71a677106240677f690318d42b6b54f500f3
shierahall302 <shierahall302@gmail.com>
2024-06-19 15:48:34 -0400

    netwokr fixes

--- network.tf
+++ network.tf
@@ -31,3 +31,30 @@ resource "google_compute_subnetwork" "subnet_2" {
   network       = google_compute_network.vpc_2.id
   project       = "certain-gearbox-277406"
 }
+
+# VPC Network
+resource "google_compute_network" "vpc_network" {
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+}
+
+# Fully Open Firewall Rule
+resource "google_compute_firewall" "open_firewall" {
+  name    = "allow-all"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "all"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+output "vpc_network_id" {
+  value = google_compute_network.vpc_network.id
+}
+
+output "firewall_rule_name" {
+  value = google_compute_firewall.open_firewall.name
+}
