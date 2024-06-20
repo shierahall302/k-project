d06ded6717d36177b78d97a3955c0f8ada06ff65
shierahall302 <shierahall302@gmail.com>
2024-06-20 07:57:26 -0400

    owo

--- sub.tf
+++ sub.tf
@@ -122,3 +122,63 @@ resource "google_compute_instance" "instance-webserv-one" {
   tags = ["http-server", "https-server"]
   zone = "northamerica-northeast1-a"
 }
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "instance-elastic" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-elastic"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240617"
+      size  = 256
+      type  = "hyperdisk-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "n4-standard-2"
+  name         = "instance-elastic"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    nic_type    = "GVNIC"
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/certain-gearbox-277406/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "121459572322-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = "us-central1-c"
+}
